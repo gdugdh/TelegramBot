a1e0c56105ad4db365566538004bf9d3db02cb04
gdugdh24 <gdugdh24@gmail.com>
2021-03-23 12:36:05 +0900

    First commit Telegram bot

---
+++ my_bot.py
@@ -0,0 +1,24 @@
+import telebot
+
+
+token = "1734346559:AAH6P66CAkvJUdjPpbidffkOD3DkqGLDsxo"
+url = "tg://resolve?domain=gdugdhBot"
+
+
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, f'Я бот. Приятно познакомиться, {message.from_user.first_name}')
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.from_user.id, 'Привет!')
+    else:
+        bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
+
+
+bot.polling(none_stop=True)
